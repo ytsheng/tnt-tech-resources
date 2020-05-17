1e93778d45fed658f3a4a9f2c8c3e6ab90f359cb
Sasha Sheng <sash@fb.com>
2020-05-17 14:58:51 -0700

    added intro text and header text

--- src/App.js
+++ src/App.js
@@ -31,10 +31,16 @@ class App extends React.Component {
     return (
       <div>
         <div>
-          <img src={logo} alt="logo" />
+          <a href="https://www.testandtrace.com"><img src={logo} alt="logo" /></a>
+          <h1 className="App-header">The Best Contact Tracing Tech Resources</h1>
         </div>
+        <br/>
+        <br/>
         <div className="App-content">
-          <h1></h1>
+          <p className="App-header-intro">Entrepreneurs, app developers, government agencies and more are working on a range of technologies for containing the COVID-19 outbreak in the United States.They include mobile and web-based solutions designed to facilitate activities such as contact tracing and screening for symptoms of the coronavirus.</p>
+          <p className="App-header-intro">The following list covers the all technological applications that are COVID-19 related.</p>
+          <br/>
+          <br/>
           <ExploreFunctions settings={this.state} onClick={(data) => this.onExploreClick(data)}/>
           <br/>
           <br/>
