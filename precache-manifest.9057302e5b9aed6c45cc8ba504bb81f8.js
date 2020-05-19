790d433f1d8e6d6af48c2d155ff81dd80f93b2e2
Mike Cook <mike@curalate.com>
2020-05-19 15:52:09 -0700

    Updates

---
+++ precache-manifest.9057302e5b9aed6c45cc8ba504bb81f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ca5f2d7e30784d72e533d24dc13a69f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85d62b3c0833ffde90fb",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8e25d06aaf7f51e8b249",
+    "url": "/static/css/main.9b19b28e.chunk.css"
+  },
+  {
+    "revision": "85d62b3c0833ffde90fb",
+    "url": "/static/js/2.e87cac6b.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/static/js/2.e87cac6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e25d06aaf7f51e8b249",
+    "url": "/static/js/main.fef93e98.chunk.js"
+  },
+  {
+    "revision": "15bd7ebbfdaf2047911d",
+    "url": "/static/js/runtime-main.93d0208f.js"
+  }
+]);
