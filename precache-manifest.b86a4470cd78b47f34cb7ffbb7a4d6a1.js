fbf6a77ce2cf65ec48a5e309636ddd145ffdf5e3
Mike Cook <mike@curalate.com>
2020-05-19 15:41:11 -0700

    Updates

---
+++ precache-manifest.b86a4470cd78b47f34cb7ffbb7a4d6a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b70660e7df21e6234444cf596d78a8a",
+    "url": "/tnt-tech-resources/index.html"
+  },
+  {
+    "revision": "85d62b3c0833ffde90fb",
+    "url": "/tnt-tech-resources/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "bb189f028d23980462c0",
+    "url": "/tnt-tech-resources/static/css/main.9b19b28e.chunk.css"
+  },
+  {
+    "revision": "85d62b3c0833ffde90fb",
+    "url": "/tnt-tech-resources/static/js/2.e87cac6b.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/tnt-tech-resources/static/js/2.e87cac6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb189f028d23980462c0",
+    "url": "/tnt-tech-resources/static/js/main.c8d89b67.chunk.js"
+  },
+  {
+    "revision": "f68412e92986eb052bef",
+    "url": "/tnt-tech-resources/static/js/runtime-main.5ee78e9b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9057302e5b9aed6c45cc8ba504bb81f8.js"
+  "/tnt-tech-resources/precache-manifest.b86a4470cd78b47f34cb7ffbb7a4d6a1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tnt-tech-resources/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
