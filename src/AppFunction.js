2c929f4dfde8793d25e6898762c2a585945d2dd0
Jonasz Karwacki <jonaszakr@gmail.com>
2020-06-11 18:06:49 -0700

    Add tippy tooltip to function buttons with dummy title.

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -1,28 +1,37 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-tippy/dist/tippy.css';
+import { Tooltip } from 'react-tippy';
 
 class AppFunction extends React.Component {
 
   render() {
     var button;
     if (this.props.activated) {
-      button = <Button
-        className="mb-2"
-        variant="primary"
-        size="lg"
-        onClick={() => this.onClick(this.props.id)}>
-        {this.props.name}
-      </Button>
+      button = <Tooltip
+        title={this.props.name}>
+            <Button
+                className="mb-2"
+                variant="primary"
+                size="lg"
+                onClick={() => this.onClick(this.props.id)}>
+                {this.props.name}
+            </Button>
+        </Tooltip>
     } else {
-      button = <Button
-        className="mb-2"
-        variant="secondary"
-        size="lg"
-        onClick={() => this.onClick(this.props.id)}>
-          {this.props.name}
-      </Button>
+      button = <Tooltip
+        title={this.props.name}>
+            <Button
+            className="mb-2"
+            variant="secondary"
+            size="lg"
+            onClick={() => this.onClick(this.props.id)}>
+              {this.props.name}
+            </Button>
+        </Tooltip>
     }
+
     return button;
   }
 
